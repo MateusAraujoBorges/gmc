0ed6ed708ce33314cd7da96aa21f8453eac6615f
yihaoyan <yihaoyan@6e9fb2d9-15d7-6d4b-b522-be9514ac623a>
2016-07-26 23:53:01 +0000

    edit comment for concurrentDfs interfaces
    git-svn-id: svn://vsl.cis.udel.edu/gmc/trunk@84 6e9fb2d9-15d7-6d4b-b522-be9514ac623a

--- src/edu/udel/cis/vsl/gcmc/ConcurrentEnablerIF.java
+++ src/edu/udel/cis/vsl/gcmc/ConcurrentEnablerIF.java
@@ -5,43 +5,65 @@ import java.util.Iterator;
 import edu.udel.cis.vsl.gcmc.util.Pair;
 
 /**
- * An EnablerIF tells you which transitions should be explored from a given
- * state. It might need to know things about the state of the search (such as
- * what states are currently on the DFS stack). Such information can be provided
- * at creation.
- * 
- * @author Stephen F. Siegel, University of Delaware
+ * Note: Even though ConcurrentEnablerIF extends SequentialEnablerIF, but the
+ * methods (hasNext, next, peek) will not be used in a concurrentDfsSearcher. I
+ * think that will be fine since many methods can be reused.
  */
 public interface ConcurrentEnablerIF<STATE, TRANSITION, TRANSITIONSEQUENCE>
-		extends SequentialEnablerIF<STATE, TRANSITION, TRANSITIONSEQUENCE>{
+		extends SequentialEnablerIF<STATE, TRANSITION, TRANSITIONSEQUENCE> {
 
 	/**
 	 * Return the size of a TRANSITIONSEQUENCE
 	 * 
+	 * I need this method because when I spawn new threads to work on different
+	 * branches, I need to know the size of the transitionSequence.
+	 * 
 	 * @return the size of transition sequence
 	 */
 	int size(TRANSITIONSEQUENCE transitionSequence);
-	
+
 	/**
 	 * Remove TRANSITION t from TRANSITIONSEQUENCE ts
+	 * 
+	 * I need this method since I need to remove a random transition from
+	 * transitionSequence and then copy the stack and spawn new thread, but
+	 * after that I need to restore the transitionSequence.
 	 * 
 	 * @return true if ts contains t.
 	 */
 	boolean removeTransition(int id, TRANSITIONSEQUENCE ts, Pair<TRANSITION, STATE> transition);
-	
+
 	/**
-	 * Add TRANSITION t to TRANSITIONSEQUENCE ts
+	 * Add TRANSITION t into TRANSITIONSEQUENCE ts
+	 * 
+	 * I need this method because when I spawn a new
 	 * 
 	 * @return true if transitionSequence does not already contain transition.
 	 */
 	boolean addTransition(TRANSITIONSEQUENCE transitionSequence, Pair<TRANSITION, STATE> transition);
-	
+
+	/**
+	 * Add a successor state into TransitionSequence.
+	 *
+	 * Store the successor states in Transition Sequence such that when I apply
+	 * the stack proviso (all successor needs to on the stack), I don't need to
+	 * get those successor states again.
+	 */
 	void addSuccessor(TRANSITIONSEQUENCE transitionSequence, STATE state);
-	
+
+	/**
+	 * @return the iterator to iterate the successor Set in TransitionSequence.
+	 * 
+	 */
 	Iterator<STATE> successorIterator(TRANSITIONSEQUENCE transitionSequence);
-	
+
 	/**
 	 * @return the iterator of transitionSequence
+	 * 
+	 *         I need to iterate the transitionSequence to initialize the
+	 *         TransitionSelector. The original interface (hasNext(), next(),
+	 *         peek()) can not to iterate TransitionSequence without modifying
+	 *         the TransitionSequence.
 	 */
 	Iterator<Pair<TRANSITION, STATE>> iterator(TRANSITIONSEQUENCE transitionSequence);
 }
--- src/edu/udel/cis/vsl/gcmc/ConcurrentStateManagerIF.java
+++ src/edu/udel/cis/vsl/gcmc/ConcurrentStateManagerIF.java
@@ -3,25 +3,10 @@ package edu.udel.cis.vsl.gcmc;
 import edu.udel.cis.vsl.gmc.StateManagerIF;
 
 /**
- * A StateManagerIF provides part of a generic interface to a state-transition
- * system. The primary method is {@link #nextState}, which, given a state and a
- * transition, returns the "next state", i.e., the state which results from
- * executing the transition from the given state. Other methods are provided
- * that are needed specifically for depth-first search, including methods to
- * mark a state as "seen before", and to make a state as
- * "currently on (or off) the stack". Still other methods are provided for
- * printing information about states.
- * 
- * @author Stephen F. Siegel
- * 
- * @param <STATE>
- *            the type used to represent states in the state-transition system
- *            being analyzed
- * @param <TRANSITION>
- *            the type used to represent transitions in the state-transition
- *            system being analyzed
+ * Note methods setOnStack(STATE state, boolean value), onStack(STATE state)
+ * will not be used in a concurrentDfsSearcher.
  */
-public interface ConcurrentStateManagerIF<STATE, TRANSITION> extends StateManagerIF<STATE, TRANSITION>{
+public interface ConcurrentStateManagerIF<STATE, TRANSITION> extends StateManagerIF<STATE, TRANSITION> {
 
 	/**
 	 * Indicate whether a STATE is on the stack of the thread with certain id.
@@ -32,22 +17,38 @@ public interface ConcurrentStateManagerIF<STATE, TRANSITION> extends StateManage
 
 	/**
 	 * Indicate whether a STATE is fully explored (all its descendants have been
-	 * visited).
+	 * visited). This is the same with telling whether a state is colored
+	 * 'blue'.
 	 * 
 	 * @return true if all state is fully explored.
 	 */
 	boolean fullyExplored(STATE state);
-	
+
 	/**
-	 * Set "fullyExplored" field of a STATE to a certain boolean value
+	 * Set "fullyExplored" field of a STATE to a certain boolean value.
+	 * 
+	 * This is the same with setting a state 'blue'.
 	 */
 	void setFullyExplored(STATE state, boolean value);
-	
+
+	/**
+	 * Set the onStack field of a state.
+	 */
 	void setOnStack(STATE state, int id, boolean value);
-	
+
+	/**
+	 * Set the inviolable field of a state using atomic CAS operation.
+	 */
 	void setInviolableCAS(STATE state, int value);
-	
+
+	/**
+	 * Tell whether a state is inviolable or not.
+	 */
 	int isInviolable(STATE state);
-	
+
+	/**
+	 * This method is used for debug and can be replaced by setSeen(STATE,
+	 * boolean).
+	 */
 	void setSeen(int id, STATE state, boolean value);
 }
